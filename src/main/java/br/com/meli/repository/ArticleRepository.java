9b8d64eb0ac92e25bb3404bf66c311ca20e4b3f5
André Arroxellas <andre.arroxellas@mercadolivre.com>
2021-12-29 10:52:40 -0300

    Implementacao: Desserializacao
    - freeShipping na Classe Produto é boolean e JSON de carrinhos está
      String NÃO e SIM.

--- src/main/java/br/com/meli/repository/ArticleRepository.java
+++ src/main/java/br/com/meli/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package br.com.meli.repository;
 
 import br.com.meli.entity.Produto;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
@@ -14,22 +15,31 @@ import java.util.List;
 @Component
 public class ArticleRepository {
 
-  	private static final String JSON_FILE_NAME = "carrinho.json";
+	private static final String JSON_FILE_NAME = "carrinho.json";
 
 	// Serializa produtos em JSON
 	public static void salvaProdutoCarrinho(List<Produto> produtos) {
-	  try{
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.writeValue(new File(JSON_FILE_NAME), produtos);
-	  }catch(IOException e){
-		System.out.println(e.getMessage());
-	  }
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.writeValue(new File(JSON_FILE_NAME), produtos);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	// Desserializa Produtos do carrinho
-  	public List<Produto> desserializaProdutos(){
-	  // TODO : Escrever código de desserialização
-	  return null;
+	public List<Produto> desserializaProdutos() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		List<Produto> listaProdutos = new ArrayList<Produto>();
+		try {
+			listaProdutos = mapper.readValue(new File("carrinho.json"),
+					mapper.getTypeFactory().constructCollectionType(
+							List.class, Produto.class));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return listaProdutos;
 	}
 
 }
