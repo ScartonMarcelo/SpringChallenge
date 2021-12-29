3ec695c6f9170549ec378b5b8eea6da651cff1ac
Francosta08 <francisco.fcosta@mercadolivre.com>
2021-12-29 10:55:09 -0300

    Alteração no endpoint /articles

--- src/main/java/br/com/meli/controller/ProdutoController.java
+++ src/main/java/br/com/meli/controller/ProdutoController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,8 +27,41 @@ public class ProdutoController {
     private ProdutoService produtoService;
 
 @GetMapping("/articles")
-public ResponseEntity<ProdutoDTO>getProdutos()
+public List<Produto>getAll()
 {
-		return ResponseEntity.ok(ProdutoDTO.builder().build());
+   List<Produto> produtos =Arrays.asList(
+
+			 Produto.builder()
+				 .id(1)
+				 .name("Serra de Bancada")
+				 .category("Ferramentas")
+				 .brand("Black & Decker")
+				 .price(new BigDecimal(500))
+				 .quantity(7)
+				 .freeShipping(true)
+				 .prestige("****")
+				 .build(),
+			 Produto.builder()
+				 .id(2)
+				 .name("Furadeira")
+				 .category("Ferramentas")
+				 .brand("Starboard")
+				 .price(new BigDecimal(183))
+				 .quantity(4)
+				 .freeShipping(true)
+				 .prestige("****")
+				 .build(),
+			 Produto.builder()
+				 .id(3)
+				 .name("Mini Cama elastica")
+				 .category("Esportes")
+				 .brand("FORTGPRO")
+				 .price(new BigDecimal(1800))
+				 .quantity(5)
+				 .freeShipping(true)
+				 .prestige("*****")
+				 .build()
+	 );
+		return produtos;
 }
 }
