b575a378c3aa13f4101c39c5217af610f9b54d0b
Thomaz Ferreira Neves Dos Santos <thomaz.ferreira@mercadolivre.com>
2021-12-29 19:05:49 -0300

    Ajuste rota de listagem de produtos

--- src/main/java/br/com/meli/controller/ProdutoController.java
+++ src/main/java/br/com/meli/controller/ProdutoController.java
@@ -2,10 +2,13 @@ package br.com.meli.controller;
 
 
 import br.com.meli.entity.Produto;
+import br.com.meli.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,48 +17,18 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class ProdutoController{
 
-    //@Autowired
-    //private ProdutoService produtoService;
+	List<Produto> produtos = new ArrayList<Produto>();
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
 
-@GetMapping("/articles")
-public List<Produto>getAll()
-{
-   List<Produto> produtos =Arrays.asList(
-
-			 Produto.builder()
-				 .productId(1)
-				 .name("Serra de Bancada")
-				 .category("Ferramentas")
-				 .brand("Black & Decker")
-				 .price(new BigDecimal(500))
-				 .quantity(7)
-				 .freeShipping(true)
-				 .prestige("****")
-				 .build(),
-			 Produto.builder()
-				 .productId(2)
-				 .name("Furadeira")
-				 .category("Ferramentas")
-				 .brand("Starboard")
-				 .price(new BigDecimal(183))
-				 .quantity(4)
-				 .freeShipping(true)
-				 .prestige("****")
-				 .build(),
-			 Produto.builder()
-				 .productId(3)
-				 .name("Mini Cama elastica")
-				 .category("Esportes")
-				 .brand("FORTGPRO")
-				 .price(new BigDecimal(1800))
-				 .quantity(5)
-				 .freeShipping(true)
-				 .prestige("*****")
-				 .build()
-	 );
-		return produtos;
-}
+	@GetMapping("/articles")
+	public ResponseEntity<List<Produto>> getAll()
+	{
+		produtos = articleRepository.desserializaProdutos();
+		return ResponseEntity.ok(produtos);
+	}
 
 }
 
