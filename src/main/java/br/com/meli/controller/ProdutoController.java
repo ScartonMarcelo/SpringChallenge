1b0ae5f0dce12e1af714950d7bac324862d8e971
Francosta08 <francisco.fcosta@mercadolivre.com>
2021-12-29 16:30:52 -0300

    Adicionado arquivos

--- src/main/java/br/com/meli/controller/ProdutoController.java
+++ src/main/java/br/com/meli/controller/ProdutoController.java
@@ -1,6 +1,6 @@
 package br.com.meli.controller;
 
-<<<<<<< HEAD
+
 import br.com.meli.entity.Produto;
 import br.com.meli.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,27 +9,17 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-=======
-import br.com.meli.dto.ProdutoDTO;
-import br.com.meli.entity.Produto;
-import br.com.meli.service.ProdutoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
->>>>>>> 4cea93af71f0d7867ec88fdd95673b20826723cb
+
 
 @RestController
 @RequestMapping("/api/v1")
-public class ProdutoController {
+public class ProdutoController<cea93af71f0d7867ec88fdd95673b20826723cb> {
 
     @Autowired
 
     private ProdutoService produtoService;
 
-<<<<<<< HEAD
+
 @GetMapping("/articles")
 public List<Produto>getAll()
 {
@@ -68,8 +58,7 @@ public List<Produto>getAll()
 	 );
 		return produtos;
 }
-=======
->>>>>>> 4cea93af71f0d7867ec88fdd95673b20826723cb
+
 }
 
 
