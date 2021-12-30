112386eb751213ca6d98f4ff6f3a7072095ec498
André Arroxellas <andre.arroxellas@mercadolivre.com>
2021-12-29 23:33:40 -0300

    Adicionando principal lógica filtros e ordenações

--- src/main/java/br/com/meli/controller/ArticlesController.java
+++ src/main/java/br/com/meli/controller/ArticlesController.java
@@ -1,18 +1,25 @@
 package br.com.meli.controller;
 
 import br.com.meli.dto.ArticlesDTO;
+import br.com.meli.dto.ProdutoDTO;
 import br.com.meli.entity.Articles;
+import br.com.meli.entity.Produto;
 import br.com.meli.service.ArticlesService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,18 +28,38 @@ public class ArticlesController {
 	@Autowired
 	private ArticlesService articleService;
 
-
 	/**
 	 * Author: Thomaz Ferreira
+	 *
 	 * @Description Rota para cadastrar produtos
-	 * @param ArticlesDTO dto
+	 * @param ArticlesDTO          dto
 	 * @param UriComponentsBuilder uriBuilder
 	 * @return ResponseEntity<ArticlesDTO>
 	 */
-  @PostMapping("/insert-articles-request")
-  private ResponseEntity<ArticlesDTO> cadastraProduto(@RequestBody Articles articles, UriComponentsBuilder uriBuilder){
-	  articleService.salvarProdutos(articles);
-	  URI uri = uriBuilder.path("/api/v1/articles").build().toUri();
-	  return ResponseEntity.created(uri).body(ArticlesDTO.converte(articles));
-  }
+	@PostMapping("/insert-articles-request")
+	private ResponseEntity<ArticlesDTO> cadastraProduto(@RequestBody Articles articles,
+			UriComponentsBuilder uriBuilder) {
+		articleService.salvarProdutos(articles);
+		URI uri = uriBuilder.path("/api/v1/articles").build().toUri();
+		return ResponseEntity.created(uri).body(ArticlesDTO.converte(articles));
+	}
+
+	@GetMapping("/articles")
+	// @Validated
+	private ResponseEntity<List<ProdutoDTO>> getListaProdutosFiltradoOrdenado(
+			@RequestParam(value = "category", required = false) String categoryName,
+			@RequestParam(value = "product", required = false) String productName,
+			@RequestParam(value = "brand", required = false) String brandName,
+			@RequestParam(value = "freeShipping", required = false) Boolean freeShipping,
+			@RequestParam(value = "order", required = false) Integer orderFilter,
+			// @RequestParam(value = "order", required = false) @Max(3) Integer orderFilter,
+			// Depends on javax.validation.constraints
+			UriComponentsBuilder uriBuilder) {
+		URI uri = uriBuilder.path("/api/v1/articles").build().toUri();
+		List<Produto> articles = ArticlesService.trateRequest(categoryName, productName, brandName, freeShipping,
+				orderFilter);
+		return ResponseEntity.created(uri).body(articles.stream()
+				.map(ProdutoDTO::converte)
+				.collect(Collectors.toList()));
+	}
 }
--- src/main/java/br/com/meli/service/ArticlesService.java
+++ src/main/java/br/com/meli/service/ArticlesService.java
@@ -3,10 +3,15 @@ package br.com.meli.service;
 import br.com.meli.entity.Articles;
 import br.com.meli.entity.Produto;
 import br.com.meli.repository.ArticleRepository;
+import br.com.meli.util.OrdenadorProdutos;
+import br.com.meli.util.OrdenadorProdutos.Filtro;
+import br.com.meli.util.OrdenadorProdutos.Ordenador;
+import br.com.meli.util.OrdenadorProdutos.Shipping;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 public class ArticlesService {
@@ -14,7 +19,46 @@ public class ArticlesService {
 	@Autowired
 	private ArticleRepository articlesRepository;
 
-	public void salvarProdutos(Articles articles){
+	public void salvarProdutos(Articles articles) {
 		articlesRepository.serializaProdutos(articles.getArticles());
+	}
+
+	public static List<Produto> trateRequest(String categoryName, String productName,
+			String brandName, Boolean freeShipping, Integer orderFilter) {
+
+		// TODO: por que static ?
+		ArticleRepository articleRepository = new ArticleRepository();
+		List<Produto> listaProdutos = articleRepository.desserializaProdutos();
+
+		OrdenadorProdutos ordenadorProdutos = new OrdenadorProdutos();
+
+		// for (int i = 0; i < Filter.values().length; i++)
+		if (orderFilter <= 3 && orderFilter >= 0) {
+			listaProdutos = ordenadorProdutos.odernarProdutos(
+					listaProdutos, null, Ordenador.values()[orderFilter]);
+		}
+
+		if (freeShipping.equals(true)) {
+			listaProdutos = ordenadorProdutos.filtrarShipping(
+					listaProdutos, null, Shipping.FILTRA_FREE_SHIPPING);
+		} else if (freeShipping.equals(false)) {
+			listaProdutos = ordenadorProdutos.filtrarShipping(
+					listaProdutos, null, Shipping.FILTRA_NON_FREE_SHIPPING);
+		}
+
+		if (!categoryName.isBlank()) {
+			listaProdutos = ordenadorProdutos.filtrarProdutos(
+					listaProdutos, categoryName, Filtro.FILTRA_CATEGORY_NAME);
+		}
+		if (!productName.isBlank()) {
+			listaProdutos = ordenadorProdutos.filtrarProdutos(
+					listaProdutos, productName, Filtro.FILTRA_PRODUCT_NAME);
+		}
+		if (!brandName.isBlank()) {
+			listaProdutos = ordenadorProdutos.filtrarProdutos(
+					listaProdutos, brandName, Filtro.FILTRA_BRAND_NAME);
+		}
+
+		return listaProdutos;
 	}
 }
