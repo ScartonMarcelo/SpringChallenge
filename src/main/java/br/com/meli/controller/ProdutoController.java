6a7bbe3911e8ac27df8f0091797a30da1d98d6db
André Arroxellas <andre.arroxellas@mercadolivre.com>
2021-12-30 18:22:56 -0300

    sanatizing code: nome e instância de articlesService & imports

--- src/main/java/br/com/meli/controller/ProdutoController.java
+++ src/main/java/br/com/meli/controller/ProdutoController.java
@@ -1,35 +1,25 @@
 package br.com.meli.controller;
 
-import br.com.meli.entity.Produto;
-import br.com.meli.service.ProdutoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-public class ProdutoController{
+public class ProdutoController {
 
-//	@Autowired
-//	private ProdutoService produtoService;
-//
+	// @Autowired
+	// private ProdutoService produtoService;
+	//
 	/**
 	 * @param
 	 * @return ResponseEntitu<< List>ProdutoDTO>>
 	 * @author thiago campos
 	 */
-//	@GetMapping("/category")
-//	public ResponseEntity<List<Produto>> getListByCategory(@RequestParam String categoryName) {
-//		List<Produto> filteredProducts = produtoService.filterByCategory(categoryName);
-//		return ResponseEntity.ok(filteredProducts);
-//	}
+	// @GetMapping("/category")
+	// public ResponseEntity<List<Produto>> getListByCategory(@RequestParam String
+	// categoryName) {
+	// List<Produto> filteredProducts =
+	// produtoService.filterByCategory(categoryName);
+	// return ResponseEntity.ok(filteredProducts);
+	// }
 }
-
-
-
-
