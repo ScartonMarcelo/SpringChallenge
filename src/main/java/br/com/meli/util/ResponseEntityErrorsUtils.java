0953c84c0f76474f3398fb842f92b9d58fbb2ec0
Thomaz Ferreira Neves Dos Santos <thomaz.ferreira@mercadolivre.com>
2021-12-31 13:11:17 -0300

    Overload responseEntityFatory

--- src/main/java/br/com/meli/util/ResponseEntityErrorsUtils.java
+++ src/main/java/br/com/meli/util/ResponseEntityErrorsUtils.java
@@ -35,4 +35,31 @@ public class ResponseEntityErrorsUtils {
 			return ResponseEntity.internalServerError().body(objectResponse);
 		}
 	}
+
+
+	/**
+	 * @author Thomaz Ferreira
+	 * @description Retorna ResponseEntity correspondente ao statusCode de erro informado
+	 * @param exceptionMsg
+	 * @param statusCode
+	 * @return ResponseEntity
+	 */
+	public ResponseEntity<?> responseEntityFactory(String customMsg, String exceptionMsg, String statusCode){
+
+		objectResponse.put("mensagem", customMsg);
+		objectResponse.put("exception", exceptionMsg);
+
+		if(statusCode.equals("400")){
+			objectResponse.put("statusCode", Integer.toString(HttpStatus.BAD_REQUEST.value()));
+			return ResponseEntity.badRequest().body(objectResponse);
+		}
+		else if(statusCode.equals("404")){
+			objectResponse.put("statusCode", Integer.toString(HttpStatus.NOT_FOUND.value()));
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(objectResponse);
+		}
+		else{
+			objectResponse.put("statusCode", Integer.toString(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+			return ResponseEntity.internalServerError().body(objectResponse);
+		}
+	}
 }
