05700e15fe1b9ee2a97f9cc1ad0e52a0f83f5616
Marcelo Scarton <marcelo.scarton@mercadolivre.com>
2022-01-02 23:29:48 -0300

    Adicionando Carrinho

---
+++ src/main/java/br/com/meli/entity/Carrinho.java
@@ -0,0 +1,11 @@
+package br.com.meli.entity;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrinho {
+	@NonNull
+	public static List<Produto> produtos = new ArrayList<>();
+}
